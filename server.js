5572f890548cd635f093ddb74185a2eff9a62abf
Pankaj Goyal <pankaj.goyal@optym.com>
2018-01-03 17:27:11 +0530

    Added Sorting and Filtering POST API

--- server.js
+++ server.js
@@ -45,33 +45,112 @@ router.get('/dispatches', function (req, res) {
     });
 });
 
+// router.get('/drivers', function (req, res) {
+//     var response = {};
+//     mongo.driver.find()
+//     .populate({ path: 'planDispatches', populate: { path: 'manifest', model: mongo.manifest}})
+//     .exec(function(err, data){
+//             if(err){
+//                 response = {"error" : true, "message" : "Error fetching data"};
+//             }
+//             else{
+//                 response = data;
+//             }
+//             res.json(response);
+//     });
+// });
+
+// router.get('/driver', function (req, res) {
+//     var response = {};
+//     mongo.driver.findOne({'driverNumber': req.query['driverNumber']})
+//     .populate({ path: 'planDispatches', populate: { path: 'manifest', model: mongo.manifest}})
+//     .exec(function(err, data){
+//             if(err){
+//                 response = {"error" : true, "message" : "Error fetching data"};
+//             }
+//             else{
+//                 response = data;
+//             }
+//             res.json(response);
+//     });
+// });
+
 router.get('/drivers', function (req, res) {
     var response = {};
-    mongo.driver.find()
+    var queryForMongoose = {};
+    
+    for(var key in req.query){
+        req.query[key] !== "" ? queryForMongoose[key] = req.query[key] : null;
+    }
+
+    mongo.driver.find(queryForMongoose)
     .populate({ path: 'planDispatches', populate: { path: 'manifest', model: mongo.manifest}})
     .exec(function(err, data){
-            if(err){
-                response = {"error" : true, "message" : "Error fetching data"};
-            }
-            else{
-                response = data;
-            }
-            res.json(response);
+        if(err){
+            response = {"error" : true, "message" : "Error fetching data"};
+        }
+        else{
+            response = data;
+        }
+        res.json(response);
     });
 });
 
-router.get('/driver', function (req, res) {
+router.post('/drivers', function (req, res) {
     var response = {};
-    mongo.driver.findOne({'driverNumber': req.query['driverNumber']})
+    var queryFilter = {};
+    var querySort = {};
+    
+    req.body.filters.forEach(function(filter){
+        var column = filter.column;
+        var operator = filter.operator;
+        var suppliedValues = filter.suppliedValues;
+
+        if(operator == 'equals'){
+            queryFilter[column] = { $in : suppliedValues };
+        }
+        else if(operator == 'notequals'){
+            queryFilter[column] = { $nin : suppliedValues };
+        }
+        else if(operator == 'between'){
+            queryFilter[column] = { $gt : suppliedValues[0], $lt : suppliedValues[1] };
+        }
+        else if(operator == '>='){
+            queryFilter[column] = { $gte : suppliedValues[0] };
+        }
+        else if(operator == '>'){
+            queryFilter[column] = { $gt : suppliedValues[0] };
+        }
+        else if(operator == '<='){
+            queryFilter[column] = { $lte : suppliedValues[0] };
+        }
+        else if(operator == '<'){
+            queryFilter[column] = { $lt : suppliedValues[0] };
+        }
+        else if(operator == 'like'){
+            queryFilter[column] = { $regex : '.*' + suppliedValues[0] + '.*', $options : 'i' };
+        }
+    });
+
+    req.body.sorts.forEach(function(sort){
+        var column = sort.column;
+        var isAscending = sort.isAscending == true ? 1 : 0;
+
+        querySort[column] = isAscending;
+    });
+
+    mongo.driver
+    .find(queryFilter)
+    .sort(querySort)
     .populate({ path: 'planDispatches', populate: { path: 'manifest', model: mongo.manifest}})
     .exec(function(err, data){
-            if(err){
-                response = {"error" : true, "message" : "Error fetching data"};
-            }
-            else{
-                response = data;
-            }
-            res.json(response);
+        if(err){
+            response = {"error" : true, "message" : "Error fetching data"};
+        }
+        else{
+            response = data;
+        }
+        res.json(response);
     });
 });
 
